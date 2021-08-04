7adcacffad36aa5440e177bef5cf065b094ced0e
jimymltta <marlettajimy@gmail.com>
2021-08-04 18:26:42 +0200

    initial

---
+++ Routes/comics.js
@@ -0,0 +1,18 @@
+// Express call and routers initialization
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+// Route 1 : get a list of comics
+router.get("/comics", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `${process.env.API_URL}/comics?apiKey=${process.env.API_KEY}`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,28 @@
+// ExpressJS, Express Formidable and Mongoose import
+require("dotenv").config();
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// Server's initialization
+const app = express();
+app.use(formidable());
+app.use(cors());
+
+// TODO
+// Connexion to the database
+
+// Routes import
+const comicsRoutes = require("./Routes/comics");
+app.use(comicsRoutes);
+
+// Undefined routes management
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Page not found" });
+});
+
+// Listen to a port (4000)
+app.listen(4000, () => {
+  console.log("⚡️ Server started ⚡️");
+});
